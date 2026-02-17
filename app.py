ae84fed57766e0b8e1a1b00d8133521c2a01be6d
CristinaGVO <vargasosoriocristina@gmail.com>
2026-02-16 19:04:28 -0800

    add update readme

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.register_blueprint(pregnancy_profile_blueprint)
 
 @app.route("/")
 def index():
-    return "Pregnancy Planner API"
+    return "Pregnancy Planner"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run()
